d2551764dca1ec70b227a00e419fc6e4f6f2afe0
Kamil Wolak <kamil.wolak@sabre.com>
2021-05-29 12:58:31 +0200

    Create Repository component

---
+++ src/components/Repository/Repository.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton, Paper, Typography } from '@material-ui/core';
+import NavigateNext from '@material-ui/icons/NavigateNext';
+import UserAvatar from '../UserAvatar/UserAvatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    padding: theme.spacing(1),
+    alignItems: 'center',
+    cursor: 'pointer',
+    '& > :not(:last-child)': {
+      marginRight: theme.spacing(1),
+    },
+  },
+  fullName: {
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+  },
+  nextButton: {
+    marginLeft: 'auto',
+  },
+}));
+
+const Repository = ({ item }) => {
+  const styles = useStyles();
+
+  return (
+    <Paper variant="outlined" className={styles.root}>
+      <UserAvatar logoUrl={item.owner.avatarUrl} />
+      <Typography className={styles.fullName}>{item.fullName}</Typography>
+      <IconButton className={styles.nextButton}>
+        <NavigateNext />
+      </IconButton>
+    </Paper>
+  );
+};
+
+Repository.propTypes = {
+  item: PropTypes.object,
+};
+
+export default Repository;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import MainLayout from './MainLayout/MainLayout';
 import Search from './Search/Search';
+import Repository from './Repository/Repository';
+import UserAvatar from './UserAvatar/UserAvatar';
 
-export { MainLayout, Search };
+export { MainLayout, Search, Repository, UserAvatar };
